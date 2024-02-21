5a40d95ffaaaaa8963061a174874f0a8623b58af
IT learning <yo7504@naver.com>
2024-02-21 17:27:31 +0900

    Feat: 라이브러리 세팅

--- Sources/GenericPresentingSwiftUIView/GenericPresentingSwiftUIView.swift
+++ Sources/GenericPresentingSwiftUIView/GenericPresentingSwiftUIView.swift
@@ -1,2 +1,75 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+class GenericHostingController<T: View>: UIHostingController<T> {
+    override init(rootView: T) {
+        super.init(rootView: rootView)
+    }
+
+    @objc required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+@available(iOS 13.0, *)
+struct GenericPresentingSwiftUIView<T: View>: UIViewRepresentable {
+    
+    private var view: GenericHostingController<AnyView>?
+
+    func makeUIView(context: Context) -> UIView {
+        return UIView()
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {
+        // 아무 업데이트가 필요하지 않을 때는 비워둡니다.
+    }
+    
+    public mutating func show(content: T) {
+        self.view = GenericHostingController(rootView: AnyView(content))
+        
+        guard let view = view else { return }
+        
+        view.view.alpha = 0.0
+        view.view.backgroundColor = .clear
+        
+        guard let window = UIApplication.shared.windows.filter({$0.isKeyWindow}).first else { return }
+        window.addSubview(view.view)
+        
+        view.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            view.view.leadingAnchor.constraint(equalTo: window.leadingAnchor),
+            view.view.trailingAnchor.constraint(equalTo: window.trailingAnchor),
+            view.view.topAnchor.constraint(equalTo: window.topAnchor),
+            view.view.bottomAnchor.constraint(equalTo: window.bottomAnchor)
+        ])
+        
+        
+        presentAnimate(dismiss: false)
+    }
+    
+    private func presentAnimate(dismiss: Bool = true, delay: CGFloat = 0.0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+            UIView.animate(withDuration: 0.0, animations: {
+                self.view?.view.alpha = 1.0
+            })
+            
+            if dismiss {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+                    self.hide()
+                })
+            }
+        }
+    }
+    
+    public func hide(delay: CGFloat = 0.3) {
+        UIView.animate(withDuration: delay, animations: {
+            self.view?.view.alpha = 0.0
+        }, completion: { _ in
+            self.view?.view.removeFromSuperview()
+        })
+    }
+}
+
--- Tests/GenericPresentingSwiftUIViewTests/GenericPresentingSwiftUIViewTests.swift
+++ Tests/GenericPresentingSwiftUIViewTests/GenericPresentingSwiftUIViewTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import SwiftUI
 @testable import GenericPresentingSwiftUIView
 
 final class GenericPresentingSwiftUIViewTests: XCTestCase {
@@ -6,6 +7,14 @@ final class GenericPresentingSwiftUIViewTests: XCTestCase {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
 
+        if #available(iOS 13.0, *) {
+            var view = GenericPresentingSwiftUIView<AnyView>()
+            view.show(content: AnyView(Text("dasd")))
+        } else {
+            // Fallback on earlier versions
+        }
+        
+        
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
